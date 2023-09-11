b7fb127b07dfc747aa7fbceb5bc11f0dae040e10
ninh <tranquangninh2001@gmail.com>
2023-09-11 19:47:31 +0700

    add ressour for day 04

--- buoi1/nhietdo.py
+++ buoi1/nhietdo.py
@@ -1,3 +1,6 @@
-C = int(input("nhập nhiệt muốn chuyển đổi:"))
-F = (C * 1.8) + 32
-print(F)
+# C = int(input("nhập nhiệt muốn chuyển đổi:"))
+# F = (C * 1.8) + 32
+# print(F)
+
+a = 1 + 2 ** 3 * 4
+print(a)
