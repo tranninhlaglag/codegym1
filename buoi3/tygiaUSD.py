5f616ac22ce0770669773e81d8385c7e1886daea
tranninhlaglag <135092699+tranninhlaglag@users.noreply.github.com>
2023-09-13 10:22:48 +0700

    Add files via upload

---
+++ buoi3/tygiaUSD.py
@@ -0,0 +1,6 @@
+
+USD_can_doi = int(input("mời nhập số $ :"))
+## giá $ ngày 13/9/2023
+gia_USD = 24.145 # vnd
+gia_quy_doi = USD_can_doi * gia_USD
+print("số tiền bạn muốn quy đôi là:",gia_quy_doi,"vnd")
